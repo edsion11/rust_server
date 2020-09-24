eef717d56747682372cd478dd3fc9f591900d1de
edsion11 <1447424545@qq.com>
2020-09-24 12:45:22 +0800

    init1

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("<h1>hello</h1>")
 }
 
 #[post("/echo")]
@@ -21,7 +21,7 @@ async fn main() -> std::io::Result<()> {
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("127.0.0.1:8000")?
     .run()
     .await
 }
